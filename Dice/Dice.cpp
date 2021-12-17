20a156b216e729cbbb5dfef2244db26f0f9a1797
Ilya Shevchenko <illyashev2905@gmail.com>
2021-12-17 13:53:22 +0200

    final version

--- Dice/Dice.cpp
+++ Dice/Dice.cpp
@@ -7,11 +7,13 @@ int main()
     srand(time(0));
     c_Dice dice;
     c_Staff_Dice staff(4);
-    std::cout << staff;
+    std::cout << "ALL DICE\n";
+    std::cout <<"\t"<< staff;
+    std::cout << "\n\nDICE BY INDEX\n";
+    std::cout <<"\t"<< staff[2];
+    std::cout << "\n\nSUMA DICE\n";
+    std::cout<<"\t"<<staff.Sum(dice);
     std::cout << "\n\n";
-   // std::cout << staff[2];
-    std::cout << "\n\n";
-    std::cout<<staff.Sum(dice);
 }
 
 
